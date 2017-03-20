71145d48324ab926b68295f06f30c2ed6aa03da4
Samson Kapeyi <skapeyi@bitbucket.org>
2017-03-20 11:09:46 +0300

    Fixed blogs

--- resources/views/admin/blogs/all.blade.php
+++ resources/views/admin/blogs/all.blade.php
@@ -2,7 +2,56 @@
 
 @section('content')
   <div class="page-header">
-    <h3>Blog Posts</h3>
+    <h3>Blogs</h3>
+  </div>
+
+  <div class="panel panel-default">
+      <div class="panel-heading clearfix">
+          <div class="btn-group pull-right">
+              <a href="/articles/create"  class="btn btn-primary btn-sm">
+                  Add Blog
+              </a>
+          </div>
+      </div>
+
+      <div class="panel-body">
+          <table class="table table-striped" id="users-table">
+              <thead>
+              <tr>
+                  <th>#</th>
+                  <th>Title</th>
+                  <th>Content</th>
+                  <th>Created</th>
+                  <th>Action</th>
+
+              </tr>
+              </thead>
+              <tbody>
+              @php
+              $i = 1
+              @endphp
+
+              @foreach($blogs as $blog)
+                  <tr>
+                      <td>{{$i}}</td>
+                      <td>{{$blog["title"]}}</td>
+                      <td>{!! $blog["content"] !!}</td>
+                      <td>{{date('d/M/y',strtotime($blog["created_at"]))}}</td>
+                      <td>
+                          <p data-placement="top" data-toggle="tooltip" title="Delete">
+                              <button class="btn btn-danger btn-xs" data-title="Delete"><span
+                                          class="glyphicon glyphicon-trash"></span> Delete
+                              </button>
+                          </p>
+                      </td>
+                  </tr>
+                  @php
+                  $i++;
+                  @endphp
+              @endforeach
+              </tbody>
+          </table>
+      </div>
   </div>
 
 @endsection
